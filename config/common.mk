fef991eb7aa180699dee9b0961fab279f6504fe4
Alberto Ponces <ponces26@gmail.com>
2023-11-26 12:53:36 +0100

    config: Switch to AOSPA's face unlock implementation
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I7b995ddb6fb54ab77e52adc18a5d76b790f566c0

--- config/common.mk
+++ config/common.mk
@@ -57,6 +57,24 @@ else
 include vendor/everest/config/vanilla.mk
 endif
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
+# Enforce privapp-permissions whitelist
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.control_privapp_permissions=enforce
+
 # Gboard side padding
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.kb_pad_port_l=4 \
